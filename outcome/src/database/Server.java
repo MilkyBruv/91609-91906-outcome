777a0cda9291e8363651dd09fa9a8a81eff8f0ce
404-WasFound <malachi.mcenroe@gmail.com>
2023-09-13 10:34:01 +1200

    started implementing tcp server database

---
+++ outcome/src/database/Server.java
@@ -0,0 +1,18 @@
+package database;
+
+import java.net.ServerSocket;
+
+public final class Server {
+    
+    public static final int port = 8080;
+    public static final String address = "127.0.0.1";
+
+    private static ServerSocket server = null;
+
+    public static void start() {
+
+        // 
+
+    }
+
+}
