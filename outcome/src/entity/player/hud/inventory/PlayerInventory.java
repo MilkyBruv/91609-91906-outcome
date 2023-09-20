55c1990bc3ce37226e9c1960dabe61dafab40465
404-WasFound <malachi.mcenroe@gmail.com>
2023-09-20 21:57:27 +1200

    fixed formatting in inventory system code

--- outcome/src/entity/player/hud/inventory/PlayerInventory.java
+++ outcome/src/entity/player/hud/inventory/PlayerInventory.java
@@ -1,13 +1,14 @@
 package entity.player.hud.inventory;
 
 import entity.item.Item;
+import entity.item.ItemList;
 import entity.player.Player;
 
 public class PlayerInventory {
     
     private Player player;
     
-    public Item[] items = new Item[2];
+    public ItemList items = new ItemList();
 
     public PlayerInventory(Player player) {
 
